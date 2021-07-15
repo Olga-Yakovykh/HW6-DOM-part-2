b5f43a808bfdd19c2cc6d0d1b4e98e8387f4e5f8
Olga <mega.denisenko@ukr.net>
2021-07-15 10:13:55 +0300

    fixHW

--- script.js
+++ script.js
@@ -1,37 +1,28 @@
+let inp = document.querySelector('input');
 
-const select = document.querySelector('.selectedElement');
-let selectOption = document.querySelector('.selectedElement').value;
-let figure = document.querySelector('div');
+let btn = document.querySelector('button');
 
+btn.addEventListener('click', () => {
 
+let result = inp.value;
 
-select.addEventListener('change', function () {
-    let style1 = this.value === 'square' ? 'display' : 'none'; 
-    document.getElementById('resultSquare').style.display = style1;
-    let style2 = this.value === 'rectangle' ? 'display' : 'none';
-    document.getElementById('resultRectangle').style.display = style2;
-    let style3 = this.value === 'circle' ? 'display' : 'none';
-    document.getElementById('resultCircle').style.display = style3;
-  
+document.querySelector('div').style.background = result;
+
 });
 
-function myFunction() {
-    document.getElementById("myInp").value = "red";
-  }
+
+let select = document.querySelector ('#selectedElement');
+const figure = document.querySelector ('#wrapper');
+
+select.addEventListener ('change', (event) => {
+
+    figure.className = event.target.value;
+})
 
 
 
-document.getElementById('btn').onclick = function (){
 
-let color = document.getElementById("myInp").value;
 
-   if (color === 'red') {
-    figure.style.backgroundColor = "#ff0000";       
-   }
-   else {
-    figure.style.backgroundColor = "#00000";
-   }
-};
 
 
 
